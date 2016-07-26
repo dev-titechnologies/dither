027441c2902f27017adf879c405d0a8b52ebea36
Titto <titto.xavier@titechnologies.org>
2016-07-26 18:44:43 +0530

    addUserContacts api in addressBook controller

--- api/controllers/AddressBookController.js
+++ api/controllers/AddressBookController.js
@@ -14,41 +14,194 @@ module.exports = {
                 var tokenCheck                  =     req.options.tokenCheck;
                 var userId                      =     tokenCheck.tokenDetails.userId;
                 console.log("addUserContacts=-=============");
-var fbUser = [
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('fb_id')},
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('fb_id')},
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('fb_id')},
-        ];
-var phonecontacts = [
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('phone_number')},
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('phone_number')},
-        {ditherUserName: req.param('user_name'),ditherPhoneNumber: req.param('phone_number')},
-        ];
-        phonecontacts.forEach(function(factor, index){
-             console.log("factor");
-             console.log(factor);
-        });
+                var query,
+                    ditherUserInAddressBook,
+                    ditherUserInFbFriends;
+                var phoneContactsArray      = [];
+                var fbUserArray             = [];
+                var fbUser                  = [
+                                                {ditherUserName: 'fb_sasi 1',fbId: 'trweewtew'},
+                                                {ditherUserName: 'fb_sasi 2',fbId: 'eeeeeeeeeee'},
+                                                {ditherUserName: 'fb_sasi 3',fbId: 'ggggggggggg'},
+                                              ];
+                var phonecontacts           = [
+                        {ditherUserName: 'sasi 1',ditherPhoneNumber: 98455454},
+                        {ditherUserName: 'sasi 2',ditherPhoneNumber: 98455454},
+                        {ditherUserName: 'sasi 3',ditherPhoneNumber: 98455454},
+                        ];
 
-        fbUser.forEach(function(factor, index){
-             console.log("factor");
-             console.log(factor);
-        });
 
-               /* var values = {
-                        userId              : userId,
-                        ditherId            : ,
-                        ditherUserName      : req.param('user_name'),
-                        ditherPhoneNumber   : req.param('phone_number'),
-                };
-            AddressBook.create(values).exec(function(err, results){
-                    if(err){
-                            console.log(err);
-                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in addressBook creation', error_details: err});
-                    }
-                    else{
+                phonecontacts.forEach(function(factor, index){
+                     console.log("factor");
+                     console.log(factor);
+                     phoneContactsArray.push("("+userId+",'"+factor.ditherUserName+"', "+factor.ditherPhoneNumber+", now(), now())");
+                });
 
-                    }
-            });*/
+                fbUser.forEach(function(factor, index){
+                     console.log("factor");
+                     console.log(factor);
+                     fbUserArray.push("("+userId+",'"+factor.ditherUserName+"', '"+factor.fbId+"', now(), now())");
+                });
+
+
+                console.log(phoneContactsArray);
+
+            async.series([
+
+                      function(callback) {
+
+                            //Parallel for insert users in addressBook and in fbFriends simultaneously
+                            async.parallel([
+                                    function(callback) {
+                                                if(phonecontacts.length != 0){
+                                                        var query = "INSERT INTO addressBook"+
+                                                                    " (userId,  ditherUserName, ditherPhoneNumber, createdAt, updatedAt)"+
+                                                                    " VALUES"+phoneContactsArray;
+
+                                                        console.log(query);
+                                                        AddressBook.query(query, function(err, createdAddressBook) {
+                                                                if(err)
+                                                                {
+                                                                    console.log(err);
+                                                                    //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in collage Detail creation', error_details: err});
+                                                                    //callback();
+                                                                    callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in contact address creation', error_details: err});
+                                                                }
+                                                                else
+                                                                {
+
+                                                                    console.log(createdAddressBook);
+                                                                    console.log("createdAddressBook ?????????????????????????????????????????????");
+                                                                    callback();
+                                                                }
+                                                        });
+                                                }else{
+                                                            callback();
+                                                }
+                                    },
+                                    function(callback) {
+                                                if(fbUser.length != 0){
+                                                        var query = "INSERT INTO fbFriends"+
+                                                                    " (userId,  ditherUserName, fbId, createdAt, updatedAt)"+
+                                                                    " VALUES"+fbUserArray;
+
+                                                        console.log(query);
+                                                        FbFriends.query(query, function(err, createdFbFriends) {
+                                                                if(err)
+                                                                {
+                                                                    console.log(err);
+                                                                    //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in collage Detail creation', error_details: err});
+                                                                    //callback();
+                                                                    callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in fbFriend creation', error_details: err});
+                                                                }
+                                                                else
+                                                                {
+
+                                                                       console.log(createdFbFriends);
+                                                                       console.log("createdFbFriends ?????????????????????????????????????????????");
+                                                                       callback();
+                                                                }
+                                                        });
+                                                }
+                                                else{
+
+                                                    callback();
+                                                }
+
+
+                                    }
+                               ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+                                        if (err) {
+                                                console.log(err);
+                                                //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in add User contact', error_details: err});
+                                                callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in address book creation or in fbFriend creation', error_details: err});
+                                        }else{
+                                                //return res.json(200, {status: 1, status_type: 'Success' ,message: 'Successfully added phone contact list to addressBook and fbcontacts to fbFriends'});
+                                                callback();
+                                        }
+
+                            });
+                    },
+                    function(callback) {
+
+
+                                    console.log("Series  -- 2nd ");
+                                    //Parallel for insert users in addressBook and in fbFriends simultaneously
+                                    async.parallel([
+                                            function(callback) {
+
+                                                        query = " SELECT adb.id, usr.id, usr.name, usr.profilePic, usr.phoneNumber"+
+                                                                    " FROM addressBook adb"+
+                                                                    " INNER JOIN user usr ON usr.id = adb.ditherId"+
+                                                                    " WHERE adb.userId = "+userId+
+                                                                    " AND adb.ditherId IS NOT NULL";
+                                                        AddressBook.query(query, function(err, selectedDitherAdb) {
+                                                                if(err)
+                                                                {
+                                                                        console.log(err);
+                                                                        //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in collage Detail creation', error_details: err});
+                                                                        //callback();
+                                                                        callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Selecting dither users from Address Book', error_details: err});
+                                                                }
+                                                                else
+                                                                {
+
+                                                                        console.log(selectedDitherAdb);
+                                                                        ditherUserInAddressBook = selectedDitherAdb;
+                                                                        callback();
+                                                                }
+                                                        });
+                                            },
+                                            function(callback) {
+                                                    query = " SELECT fbf.id, usr.id, usr.name, usr.profilePic, usr.phoneNumber"+
+                                                            " FROM fbFriends fbf"+
+                                                            " INNER JOIN user usr ON usr.id = fbf.ditherId"+
+                                                            " WHERE fbf.userId = "+userId+
+                                                            " AND fbf.ditherId IS NOT NULL";
+                                                    console.log(query);
+                                                    FbFriends.query(query, function(err, selectedDitherFbf) {
+                                                            if(err)
+                                                            {
+                                                                    console.log(err);
+                                                                    //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in collage Detail creation', error_details: err});
+                                                                    //callback();
+                                                                    callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Selecting dither users from Fb Friends', error_details: err});
+                                                            }
+                                                            else
+                                                            {
+
+                                                                   console.log(selectedDitherFbf);
+                                                                   ditherUserInFbFriends = selectedDitherFbf;
+                                                                   console.log("selectedDitherFbf ++++++++++++++++++++++++++++++++++++++++++++++++");
+                                                                   callback();
+                                                            }
+                                                    });
+                                            }
+                                     ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+                                        if (err) {
+                                                console.log(err);
+                                                //return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in add User contact', error_details: err});
+                                                callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in selecting dither users from fbFriend list or from address book list', error_details: err});
+                                        }else{
+                                                //return res.json(200, {status: 1, status_type: 'Success' ,message: 'Successfully added phone contact list to addressBook and fbcontacts to fbFriends'});
+                                                console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                                                callback();
+                                        }
+
+                            });
+
+                    },
+
+                    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+                                            if (err) {
+                                                console.log(err);
+                                                return res.json(200, {status: 2, status_type: 'Failure' , message: 'Some error occured in address book creation or in fbFriend creation or getting fbfriends or grtting contacts', error_details: err}); //If an error occured, we let express/connect handle it by calling the "next" function
+                                            }else{
+                                                console.log("Success -----------------------------------------");
+                                                return res.json(200, {status: 1, status_type: 'Success' , message: 'Successfully added phone contact list to addressBook and fbcontacts to fbFriends', ditherPhoneContact: ditherUserInAddressBook, ditherFBuser: ditherUserInFbFriends});
+                                            }
+
+                });
 
         },
 };

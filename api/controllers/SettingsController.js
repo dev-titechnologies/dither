640b1bed286a371ebfb40509ab7a5d74d1a74432
Titto <titto.xavier@titechnologies.org>
2016-07-29 18:12:50 +0530

    changes in settings controller

--- api/controllers/SettingsController.js
+++ api/controllers/SettingsController.js
@@ -8,36 +8,41 @@
 module.exports = {
 
     settingsKeyValue: function (req, res, next) {
-		       console.log("ghuihg")
+                console.log("Settings Controller ===>>> settingsKeyValue")
                 var query = "SELECT * FROM settings";
                 Settings.query(query, function (err, results) {
                         if (err) {
                             callback(true, err);
+                            callback(true, {status: 2, status_type: 'Failure' ,message: 'Some error occured in select Settings values'});
                         }
                         else {
                                 //console.log(results);
-                                var count = 0;
-                                var keyArray = [];
-                                var valueArray = [];
-                                results.forEach(function (factor, index) {
-                                    count++;
-                                    keyArray.push(factor.key);
-                                    valueArray.push(factor.value);
-                                });
+                                if(results.length == 0){
+                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No settings Found', error_details: err});
+                                }else{
+                                        var count = 0;
+                                        var keyArray = [];
+                                        var valueArray = [];
+                                        results.forEach(function (factor, index) {
+                                            count++;
+                                            keyArray.push(factor.key);
+                                            valueArray.push(factor.value);
+                                        });
 
-                                var keyValue = {},
-                                        i,
-                                        keys = keyArray,
-                                        values = valueArray;
-                                //Merge 2 arrays
-                                for (i = 0; i < keys.length; i++) {
-                                    keyValue[keys[i]] = values[i];
+                                        var keyValue = {},
+                                                i,
+                                                keys = keyArray,
+                                                values = valueArray;
+                                        //Merge 2 arrays
+                                        for (i = 0; i < keys.length; i++) {
+                                            keyValue[keys[i]] = values[i];
+                                        }
+                                        //console.log("Key Value");
+                                        console.log(keyValue);
+
+                                        req.options.settingsKeyValue = keyValue;
+                                        next();
                                 }
-                                //console.log("Key Value");
-                                //console.log(keyValue);
-
-                                req.options.settingsKeyValue = keyValue;
-                                next();
                         }
 
                 });

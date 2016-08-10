92a39ea1197b7caf816ff985a697c8d1e2d5dc0d
Titto <titto.xavier@titechnologies.org>
2016-08-10 19:55:23 +0530

    check whether the user is already commented or not

--- api/controllers/CollageLikesController.js
+++ api/controllers/CollageLikesController.js
@@ -26,57 +26,102 @@ module.exports = {
                     if(!collageId || !likedImageId || !imgPosition){
                             return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Please pass the dither_id and dither_like_image_id and position'});
                     }else{
-                                var values = {
-                                    collageId       :       collageId,
-                                    imageId         :       likedImageId,
-                                    userId          :       userId,
-                                    likeStatus      :       true,
-                                    likePosition    :       imgPosition,
-                                };
-                                console.log(values);
-                                CollageLikes.create(values).exec(function(err, results){
-                                        if(err){
-                                                console.log(err);
-                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Dither Vote Creation', error_details: err});
+                                //To check the user already voted for this image or not
+                                CollageLikes.findOne({collageId: collageId, userId: userId}).exec(function (err, found){
+                                    if(err){
+                                        console.log(err);
+                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding user voted the image earlier or not', error_details: err});
+                                    }else{
+                                        if(found){
+                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'User already voted against this image'});
+                                        }else{
+                                                var values = {
+                                                    collageId       :       collageId,
+                                                    imageId         :       likedImageId,
+                                                    userId          :       userId,
+                                                    likeStatus      :       true,
+                                                    likePosition    :       imgPosition,
+                                                };
+                                                console.log(values);
+                                                //Creating the vote
+                                                CollageLikes.create(values).exec(function(err, results){
+                                                        if(err){
+                                                                console.log(err);
+                                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Dither Vote Creation', error_details: err});
+                                                        }
+                                                        else{
+                                                            console.log("results ======================");
+                                                            console.log(results);
+                                                            console.log(results.collageId);
+                                                            console.log(results.imageId);
+                                                            //To check the CollageId and the ImageId is existing or not
+                                                            CollageDetails.findOne({id: results.imageId, collageId: results.collageId}).exec(function (err, foundImgResults){
+                                                                    if(err){
+                                                                        console.log(err);
+                                                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding image from Image Details', error_details: err});
+                                                                    }else{
+                                                                        console.log(foundImgResults);
+                                                                            if(!foundImgResults){
+                                                                                    return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No Image of the Dither Found by this id'});
+                                                                            }else{
+                                                                                    var criteria = {id: foundImgResults.id};
+                                                                                    var values   = {vote: parseInt(foundImgResults.vote) + 1};
+                                                                                    //To Update the vote count against the image
+                                                                                    CollageDetails.update(criteria, values).exec(function(err, updatedVoteCount) {
+                                                                                        if(err)
+                                                                                        {
+                                                                                            console.log(err);
+                                                                                            return res.json(200, {status: 2, status_type: 'Failure', message: 'Some error has occured in Updating vote count of a single image'});
+                                                                                        }
+                                                                                        else
+                                                                                        {
+                                                                                            //To check the collage is existing or not
+                                                                                            Collage.findOne({id: results.collageId}).exec(function (err, foundCollageResults){
+                                                                                                    if(err){
+                                                                                                        console.log(err);
+                                                                                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding dither', error_details: err});
+                                                                                                    }else{
+
+                                                                                                                if(!foundCollageResults){
+                                                                                                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No Dither Found by this id'});
+                                                                                                                }else{
+
+                                                                                                                    var criteria = {id: foundCollageResults.id};
+                                                                                                                    var values   = {totalVote: parseInt(foundCollageResults.totalVote) + 1};
+                                                                                                                    //To update the totalVote (opinion) against that collage
+                                                                                                                    Collage.update(criteria, values).exec(function(err, updatedTotalVoteCount) {
+                                                                                                                            if(err)
+                                                                                                                            {
+                                                                                                                                console.log(err);
+                                                                                                                                return res.json(200, {status: 2, status_type: 'Failure', message: 'Some error has occured in Updating total opinion of a Dither'});
+                                                                                                                            }
+                                                                                                                            else
+                                                                                                                            {
+                                                                                                                                  return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully voted the Image',
+                                                                                                                                            total_like_count       :  updatedVoteCount[0].vote,
+                                                                                                                                    });
+                                                                                                                            }
+                                                                                                                    });
+
+                                                                                                                }
+                                                                                                    }
+                                                                                            });
+
+
+
+
+                                                                                        }
+                                                                                    });
+                                                                            }
+
+                                                                    }
+
+                                                            });
+
+                                                        }
+                                                });
                                         }
-                                        else{
-                                            console.log("results ======================");
-                                            console.log(results);
-                                            console.log(results.collageId);
-                                            console.log(results.imageId);
-                                            CollageDetails.findOne({id: results.imageId, collageId: results.collageId}).exec(function (err, foundImgResults){
-                                                    if(err){
-                                                        console.log(err);
-                                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding image from Image Details', error_details: err});
-                                                    }else{
-                                                        console.log(foundImgResults);
-                                                            if(!foundImgResults){
-                                                                    return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No Image of the Dither Found by this id'});
-                                                            }else{
-                                                                    var criteria = {id: foundImgResults.id};
-                                                                    var values   = {vote: parseInt(foundImgResults.vote) + 1};
-                                                                    CollageDetails.update(criteria, values).exec(function(err, updatedVoteCount) {
-                                                                        if(err)
-                                                                        {
-                                                                            console.log(err);
-                                                                            return res.json(200, {status: 2, status_type: 'Failure', message: 'Some error has occured in Updating vote count of a single image'});
-                                                                        }
-                                                                        else
-                                                                        {
-
-
-                                                                               /* return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully voted the Image',
-                                                                                                    total_like_count       :  updatedVoteCount[0].vote,
-                                                                                                });*/
-                                                                        }
-                                                                    });
-                                                            }
-
-                                                    }
-
-                                            });
-
-                                        }
+                                    }
                                 });
                     }
 

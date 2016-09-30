12308ad568f926d081a40304a00eff3e612527ca
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-09-30 16:18:48 +0530

    changes in Notification

--- api/controllers/NotificationController.js
+++ api/controllers/NotificationController.js
@@ -617,7 +617,7 @@ module.exports = {
                             }
                             else
                                 {
-                                    return res.json(200, {status: 1,status_type:"Success",msg: 'No notification found',notification_data:[]});
+                                    return res.json(200, {status: 2,status_type:"Success",msg: 'No notification found',notification_data:[]});
                                 }
                             }
                         });

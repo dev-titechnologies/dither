44a2c5f23dbe746882c603094d067b1bfea43527
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-10-13 10:46:45 +0530

    changes in collage details controller

--- api/controllers/CollageDetailsController.js
+++ api/controllers/CollageDetailsController.js
@@ -209,7 +209,22 @@ module.exports = {
                                                                             console.log(query);
                                                                             //console.log(taggedUsersFinalResults);
                                                                             var taggedUserArrayFinal = [];
-                                                                            
+                                                                            if(taggedUsersFinalResults != 0){
+                                                                                taggedUsersFinalResults.forEach(function(factor, index){
+                                                                                        //console.log("factor");
+                                                                                        //console.log(factor);
+                                                                                        if(factor.mentionId=='')
+																						{
+																							factor.mentionId = factor.ditherUserId;
+																						}
+                                                                                        taggedUserArrayFinal.push({
+                                                                                                                name            :   factor.name,
+                                                                                                                userId          :   factor.ditherUserId,
+                                                                                                                profile_image   :   profilePic_path + factor.profilePic,
+                                                                                                                mention_id		:	factor.mentionId
+                                                                                                                });
+                                                                                });
+                                                                            }
 
                                                                             query = " SELECT invt.phoneNumber, invt.invitee"+
                                                                                     " FROM invitation invt"+
@@ -279,76 +294,7 @@ module.exports = {
 																									}
 																							});	
 																						},	
-																						 function(callback) {
-																						
-																						    //------------------------------Generate ThumbnailImage-----------------------------------------------
-																								if(taggedUsersFinalResults != 0){
-																											taggedUsersFinalResults.forEach(function(factor, index){
-																													//console.log("factor");
-																													//console.log(factor);
-																													if(factor.mentionId=='')
-																													{
-																														factor.mentionId = factor.ditherUserId;
-																													}
-																													if(factor.profilePic)
-																													{
-																														var imageSrc                    =     profilePic_path_assets + factor.profilePic;
-																														var profile_image;
-																														fs.exists(imageSrc, function(exists) {
-																															 if (exists) {
-
-																																	console.log("Image exists");
-
-																																	var ext                         =     imageSrc.split('/');
-																																	ext                             =     ext[ext.length-1].split('.');
-																																	var imageDst                    =     profilePic_path_assets + ext[0] + "_50x50" + "." +ext[1];
-																																	console.log(imageSrc)
-																																	console.log(imageDst)
-																																	ImgResizeService.isImageExist(imageSrc, imageDst, function(err, imageResizeResults) {
-																																		if(err)
-																																		{
-																																			console.log("thumbNail creation error occured")
-																																			console.log(err)
-																																			callback();
-
-																																		}
-																																		else
-																																		{
-																																			console.log(imageResizeResults)
-																																			profile_image = profilePic_path + ext[0] + "_50x50" + "." +ext[1];
-																																			console.log("--------**********************************************--------")
-																																			console.log(user_profile_image)
-																																			taggedUserArrayFinal.push({
-																																				name            :   factor.name,
-																																				userId          :   factor.ditherUserId,
-																																				profile_image   :   profile_image,
-																																				mention_id		:	factor.mentionId
-																																			});
-																																			callback();
-														
-																																		}
-																																	});
-																																}
-																																else
-																																{
-																																	callback();
-																																}
-																														});	
-																														
-																													}
-																													
-																											});
-																								}
-																								else
-																								{
-																									callback();
-																								}
-																							
-																								
-																							
-
-																						},	
-
+																						 
                                                                               
 																						], function(err) { //This function gets called after the two tasks have called their "task callbacks"
                                                                                         if (err) {

ea0eed61a37b6e356695cabd34319b391628ffa8
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-10-13 11:59:05 +0530

    changes in detail controller

--- api/controllers/CollageDetailsController.js
+++ api/controllers/CollageDetailsController.js
@@ -207,6 +207,7 @@ module.exports = {
                                                                         {
 
                                                                             console.log(query);
+                                                                            var profile_image;
                                                                             //console.log(taggedUsersFinalResults);
                                                                             var taggedUserArrayFinal = [];
                                                                             if(taggedUsersFinalResults != 0){
@@ -217,10 +218,46 @@ module.exports = {
 																						{
 																							factor.mentionId = factor.ditherUserId;
 																						}
+																						if(factor.profilePic)
+																						{
+																							var imageSrc                    =     profilePic_path_assets + factor.profilePic;
+																								
+																							fs.exists(imageSrc, function(exists) {
+																								 if (exists) {
+
+																										console.log("Image exists");
+
+																										var ext                         =     imageSrc.split('/');
+																										ext                             =     ext[ext.length-1].split('.');
+																										var imageDst                    =     profilePic_path_assets + ext[0] + "_50x50" + "." +ext[1];
+																										console.log(imageSrc)
+																										console.log(imageDst)
+																										ImgResizeService.isImageExist(imageSrc, imageDst, function(err, imageResizeResults) {
+																											if(err)
+																											{
+																												console.log("thumbNail creation error occured")
+																												console.log(err)
+																												
+
+																											}
+																											else
+																											{
+																												console.log(imageResizeResults)
+																												profile_image = profilePic_path + ext[0] + "_50x50" + "." +ext[1];
+																												console.log("--------**********************************************--------")
+																												
+																												
+							
+																											}
+																										});
+																									}
+																									
+																							});	
+																						}
                                                                                         taggedUserArrayFinal.push({
                                                                                                                 name            :   factor.name,
                                                                                                                 userId          :   factor.ditherUserId,
-                                                                                                                profile_image   :   profilePic_path + factor.profilePic,
+                                                                                                                profile_image   :   profile_image,
                                                                                                                 mention_id		:	factor.mentionId
                                                                                                                 });
                                                                                 });

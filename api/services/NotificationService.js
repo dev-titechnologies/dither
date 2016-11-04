82fabbd2be94d2c769a23a94de8423b10ff91a5c
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-11-04 12:28:45 +0530

    changes in notification

--- api/services/NotificationService.js
+++ api/services/NotificationService.js
@@ -19,7 +19,7 @@ module.exports = {
 		var details ={message:data.NtfnBody,type:data.NtfnType,id:data.id,notification_id:data.notification_id};
 		console.log(details)
 		ios = PusherService('ios', {
-			device: ['5071f159ee25c3d6327f54d917bf724b9b91d36be88f3149b1735b0a473116bf'], // Array of string with device tokens
+			device: [], // Array of string with device tokens
 			provider: {
 				cert: 'assets/push_Ntfn_certificates/PushChatCert.pem', // The filename of the connection certificate to load from disk
 				key: 'assets/push_Ntfn_certificates/PushChatKey.pem', // The filename of the connection key to load from disk
@@ -45,16 +45,14 @@ module.exports = {
 				icon: '', // Indicates notification icon
 				sound: '', // Indicates sound to be played
 				badge: '1', // Indicates the badge on client app home icon
-				payload: {}, // Custom data to send within Push Notification
-				alert: "Hello from APNs Tester."
-				
+				payload: {"alert":"Hello from APNs Tester.","badge":"1"}, // Custom data to send within Push Notification
 			  }
 			});
 			
 			//console.log(ios)
 		
 			ios
-			  .send(['5071f159ee25c3d6327f54d917bf724b9b91d36be88f3149b1735b0a473116bf'], {
+			  .send([device_id], {
 				body: details
 			  })
 			  .then(console.log.bind(console))

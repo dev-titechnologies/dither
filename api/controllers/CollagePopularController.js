d72e8bcbcc1e48c70cb4583f976f866a20ac1903
Titto <titto.xavier@titechnologies.org>
2016-11-16 12:57:00 +0530

    collage Popular dither expiry check

--- api/controllers/CollagePopularController.js
+++ api/controllers/CollagePopularController.js
@@ -82,7 +82,7 @@ module.exports = {
                                         " SELECT clg.id FROM collage clg INNER JOIN tags tg ON tg.collageId = clg.id"+
                                         " WHERE"+
                                         " tg.userId =  '"+userId+"' AND clg.userId =  '"+received_userId+"' AND clg.totalVote != 0"+
-                                        //" AND clg.expiryDate > '"+today+"'"+
+                                        " AND clg.expiryDate > '"+today+"'"+
                                         " ORDER BY clg.totalVote  DESC, clg.createdAt  DESC"+
                                         " ) AS temp"+
                                         " LIMIT "+received_focus_limit_number+", "+data_view_limit+

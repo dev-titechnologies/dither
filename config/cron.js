bd49b855a6863d997f55e0ab515325ef61374dae
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-11-23 18:29:39 +0530

    design updations in mail template

--- config/cron.js
+++ config/cron.js
@@ -5,7 +5,7 @@ module.exports.cron = {
         onTick:  function (req, res, next)  {
 			
             var today = new Date().toISOString().slice(0, 19).replace('T', ' ');
-            //console.log(today)
+            console.log(today)
             Collage.find({expiryDate:today }).exec(function(err, collageDetails){
                 if(err){
                    console.log(err)

046ef9455ecfb011cc0446715ab00ce9020a3cae
Titto <titto.xavier@titechnologies.org>
2016-11-23 12:49:28 +0530

    aligned report controller

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -28,83 +28,71 @@ module.exports = {
                                 return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Please pass the report_type and description and user_id'});
 
                     }else{
-                            ReportUser.findOne({reporterId: userId, userId: received_userId}).exec(function (err, foundReport){
-                                    if(err){
+                        ReportUser.findOne({reporterId: userId, userId: received_userId}).exec(function (err, foundReport){
+                            if(err){
+                                        console.log(err);
+                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding user already reported or not ', error_details: err});
+                            }else{
+                                if(foundReport){
+                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'You have already reported this User'});
+                                }else{
+                                    var values = {
+                                            report                  :           report,
+                                            reportType              :           reportType,
+                                            reporterId              :           userId,
+                                            userId                  :           received_userId,
+                                    };
+                                    ReportUser.create(values).exec(function(err, results){
+                                        if(err){
                                                 console.log(err);
-                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding user already reported or not ', error_details: err});
-                                    }else{
-
-                                            if(foundReport){
-                                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'You have already reported this User'});
-                                            }else{
-                                                    var values = {
-                                                            report                  :           report,
-                                                            reportType              :           reportType,
-                                                            reporterId              :           userId,
-                                                            userId                  :           received_userId,
-                                                    };
-                                                    ReportUser.create(values).exec(function(err, results){
-                                                            if(err){
-                                                                    console.log(err);
-                                                                    return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in report user insertion', error_details: err});
-                                                            }
-                                                            else{
-                                                                    console.log(results);
-                                                                    //-----------------------PUSH NOTIFICATION-------------------------------------------------------------
-
-                                                                    User_token.find({userId: received_userId }).exec(function (err, getTokenDetails){
-                                                                                            if(err)
-                                                                                            {
-                                                                                                  console.log(err);
-                                                                                                  return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in findig deviceId', error_details: err});
-                                                                                            }
-                                                                                            else
-                                                                                            {
-                                                                                                console.log(getTokenDetails)
-                                                                                                var message     =  'Report User Notification';
-                                                                                                var ntfn_body   =  " Reported againt You";
-                                                                                                //var device_id   =  getTokenDetails.deviceId;
-                                                                                                var deviceId_arr = [];
-                                                                                                getTokenDetails.forEach(function(factor, index){
-
-                                                                                                                deviceId_arr.push(factor.deviceId);
-
-
-                                                                                                    });
-
-                                                                                                if(!deviceId_arr.length){
-                                                                                                         return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
-                                                                                                }else{
-                                                                                                        var data        =  {message:message,device_id:deviceId_arr,NtfnBody:ntfn_body,id:userId,NtfnType:6};
-                                                                                                        console.log(data)
-
-                                                                                                         NotificationService.NotificationPush(data, function(err, ntfnSend){
-                                                                                                            if(err){
-                                                                                                                    console.log("Error in Push Notification Sending")
-                                                                                                                    console.log(err)
-                                                                                                                    return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
-
-                                                                                                            }else{
-                                                                                                                    return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
-                                                                                                            }
-                                                                                                        });
-
-
-
-                                                                                                }
-
-                                                                                            //------------------------------
-                                                                                            }
-                                                                    });
-
-
-                                                            }
+                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in report user insertion', error_details: err});
+                                        }else{
+                                            console.log(results);
+                                            //-----------------------PUSH NOTIFICATION-------------------------------------------------------------
+                                            User_token.find({userId: received_userId }).exec(function (err, getTokenDetails){
+                                                if(err){
+                                                      console.log(err);
+                                                      return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in findig deviceId', error_details: err});
+                                                }else{
+                                                    console.log(getTokenDetails)
+                                                    var message     =  'Report User Notification';
+                                                    var ntfn_body   =  " Reported againt You";
+                                                    //var device_id   =  getTokenDetails.deviceId;
+                                                    var deviceId_arr = [];
+                                                    getTokenDetails.forEach(function(factor, index){
+                                                            deviceId_arr.push(factor.deviceId);
                                                     });
-                                            }
-                                    }
-                            });
-                    }
+                                                    if(!deviceId_arr.length){
+                                                             return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
+                                                    }else{
+                                                        var data    =   {
+                                                                    message         :       message,
+                                                                    device_id       :       deviceId_arr,
+                                                                    NtfnBody        :       ntfn_body,
+                                                                    id              :       userId,
+                                                                    NtfnType        :       6
+                                                                    };
+                                                        console.log(data)
+                                                        NotificationService.NotificationPush(data, function(err, ntfnSend){
+                                                            if(err){
+                                                                    console.log("Error in Push Notification Sending")
+                                                                    console.log(err)
+                                                                    return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
 
+                                                            }else{
+                                                                    return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the user'});
+                                                            }
+                                                        });
+                                                    }
+                                                }
+                                            });
+                                            //------------------------------
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
         },
 
 
@@ -126,49 +114,45 @@ module.exports = {
                                 return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Please pass the report_type and description and dither_id'});
 
                     }else{
-                            //To check the user already voted for this image or not
-                            Collage.findOne({id: collageId}).exec(function (err, found){
-                                if(err){
-                                            console.log(err);
-                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding collage is existing or not', error_details: err});
+                        //To check the user already voted for this image or not
+                        Collage.findOne({id: collageId}).exec(function (err, found){
+                            if(err){
+                                        console.log(err);
+                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding collage is existing or not', error_details: err});
+                            }else{
+                                if(!found){
+                                        return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No collage Found'});
                                 }else{
-                                    if(!found){
-                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'No collage Found'});
-                                    }else{
-
-                                            ReportDither.findOne({reporterId: userId, collageId: collageId}).exec(function (err, foundReport){
-                                                if(err){
+                                    ReportDither.findOne({reporterId: userId, collageId: collageId}).exec(function (err, foundReport){
+                                        if(err){
+                                                console.log(err);
+                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding user already reported or not ', error_details: err});
+                                        }else{
+                                            if(foundReport){
+                                                    return res.json(200, {status: 2, status_type: 'Failure' ,message: 'You have already reported this Dither'});
+                                            }else{
+                                                var values = {
+                                                        report                  :           report,
+                                                        reportType              :           reportType,
+                                                        reporterId              :           userId,
+                                                        collageId               :           collageId,
+                                                };
+                                                ReportDither.create(values).exec(function(err, results){
+                                                    if(err){
                                                             console.log(err);
-                                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in Finding user already reported or not ', error_details: err});
-                                                }else{
-
-                                                        if(foundReport){
-                                                                    return res.json(200, {status: 2, status_type: 'Failure' ,message: 'You have already reported this Dither'});
-                                                        }else{
-                                                                var values = {
-                                                                        report                  :           report,
-                                                                        reportType              :           reportType,
-                                                                        reporterId              :           userId,
-                                                                        collageId               :           collageId,
-                                                                };
-                                                                ReportDither.create(values).exec(function(err, results){
-                                                                        if(err){
-                                                                                console.log(err);
-                                                                                return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in report Dither insertion', error_details: err});
-                                                                        }
-                                                                        else{
-                                                                                console.log(results);
-                                                                                return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the Dither'});
-                                                                        }
-                                                                });
-                                                        }
-                                                }
-                                            });
-                                    }
+                                                            return res.json(200, {status: 2, status_type: 'Failure' ,message: 'Some error occured in report Dither insertion', error_details: err});
+                                                    }else{
+                                                            console.log(results);
+                                                            return res.json(200, {status: 1 ,status_type: 'Success', message: 'Succesfully reported against the Dither'});
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
                                 }
-                            });
+                            }
+                        });
                     }
-
         },
         /* ==================================================================================================================================
                         To Get ReportedUser List
@@ -207,7 +191,6 @@ module.exports = {
                         }
 
                     });
-
         },
 
 

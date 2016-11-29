3fcdc05c109bceac4f56cd2e012e6866b44e9808
anumol rajeev <anumol.rajeev@titechnologies.org>
2016-11-29 12:51:34 +0530

    pass old_id in push response

--- api/controllers/CollageCommentsController.js
+++ api/controllers/CollageCommentsController.js
@@ -154,6 +154,8 @@ module.exports = {
                                                                                     console.log(err)
                                                                                     callback();
                                                                                 }else{
+																					console.log("delete comment NotificationId")
+																					console.log(deleteCommentNtfn)
                                                                                     callback();
                                                                                 }
                                                                             });

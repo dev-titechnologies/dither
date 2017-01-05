068f2b9e8af79eab8d5ff15961488acc0f307aec
anumol rajeev <anumol.rajeev@titechnologies.org>
2017-01-05 12:18:28 +0530

    changes in test

--- api/controllers/TestController.js
+++ api/controllers/TestController.js
@@ -1383,7 +1383,7 @@ module.exports = {
     testResize: function (req, res){
 		
 		var im = require('imagemagick');
-		im.identify('imageNw.jpeg', function(err, features){
+		im.identify('http://git.titechnologies.in:5000/images/profilepics/ff3a4bf0-3a61-4313-8d62-d36f3f9d8db4.png', function(err, features){
 		  if (err) {
 					//throw err
 					return res.json(200, {status: 2, status_type: 'Failure', message: 'error'});

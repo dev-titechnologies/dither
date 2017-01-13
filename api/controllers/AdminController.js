637fd70b7412b79fb1cc9b1452769e195f915614
Titto <titto.xavier@titechnologies.org>
2017-01-13 10:35:06 +0530

    comment part in admin controller

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -588,12 +588,16 @@ console.log(values);
                         }
                     });
     },
-  
+
+/* ==================================================================================================================================
+               Get the comments against the dither
+   ==================================================================================================================================== */
     getComments:     function(req,res){
                         console.log("getComments   =================== ADMIN");
                         var server_image_baseUrl        =     req.options.settingsKeyValue.CDN_IMAGE_URL;
                         var profilePic_path             =     server_image_baseUrl + req.options.file_path.profilePic_path;
-                        var profile_image;
+                        var profilePic_path_assets      =     req.options.file_path.profilePic_path_assets;
+                        var profile_image, profile_image_70x70;
                         var collageId                   =     req.body.id;
                         if(req.param("count")==0){
                             var query   = " SELECT"+
@@ -614,7 +618,7 @@ console.log(values);
 
 
                         }
-                        CollageComments.query(query,function(err,result){
+                        /*CollageComments.query(query,function(err,result){
                             if(err){
                                 console.log("errrRRRRRRRRR");
                                 return res.json(200, {status: 2, error_details: err});
@@ -631,6 +635,95 @@ console.log(values);
                                 });
                                 return res.json(200,{status:1,message:'success',result:result});
                             }
+
+                        });*/
+
+                        var results             =       [];
+                        async.series([
+                                    function(callback) {
+                                                CollageComments.query(query, function (err, result){
+                                                    if(err){
+                                                        //return res.json(200, {status: 2, error_details: err});
+                                                        callback();
+                                                    }else{
+                                                            results  = result;
+                                                            callback();
+                                                    }
+                                                });
+                                    },
+                                    function(callback){
+                                                console.log("INSIDE user foreach callback........");
+                                                var count = 0;
+                                                results.forEach(function(factor, index){
+                                                        count++;
+                                                        var imageSrc                    =     profilePic_path_assets + factor.profileImage;
+                                                        var ext                         =     imageSrc.split('/');
+                                                        ext                             =     ext[ext.length-1].split('.');
+                                                        var imgWidth,
+                                                            imgHeight,
+                                                            imageDst;
+
+                                                        async.series([
+                                                                function(callback) {
+                                                                            /*imgWidth                    =    242;
+                                                                            imgHeight                   =    242;
+                                                                            imageDst                    =     collageImg_path_assets + ext[0] + "_"+imgWidth+"x"+imgHeight+"." +ext[1];
+                                                                            ImgResizeService.imageResizeWH(imgWidth, imgHeight, imageSrc, imageDst, function(err, imageResizeResults) {
+                                                                                    if(err){
+                                                                                            console.log(err);
+                                                                                            console.log("Error in image resize 160 in collagedetails!!!!");
+                                                                                            //callback();
+                                                                                    }else{
+                                                                                           // callback();
+                                                                                            console.log("Loop success");
+                                                                                            //collage-Details images
+
+                                                                                    }
+                                                                            });*/
+                                                                            callback();
+
+                                                                },
+                                                        ],function(err){
+                                                                    if(err){
+                                                                        console.log(err);
+                                                                        //callback();
+                                                                    }else{
+                                                                            if(factor.profileImage == null || factor.profileImage == ""){
+                                                                                    profile_image                   =     "";
+                                                                                    profile_image_70x70             =     "";
+                                                                            }else{
+                                                                                    var imageSrc                    =     profilePic_path_assets + factor.profileImage;
+                                                                                    var ext                         =     imageSrc.split('/');
+                                                                                    ext                             =     ext[ext.length-1].split('.');
+                                                                                    profile_image                   =     profilePic_path + factor.profileImage;
+                                                                                    profile_image_70x70             =     profilePic_path + ext[0] + "_70x70." +ext[1];
+                                                                            }
+                                                                            factor.profilePic                       =     profile_image;
+                                                                            factor.profilePic_70x70                 =     profile_image_70x70;
+                                                                            //console.log(factor.profilePic_70x70);
+                                                                            if(count == results.length){
+                                                                                    callback();
+                                                                            }
+                                                                    }
+                                                        });
+
+
+                                                });
+
+                                    },
+                        ],function(err){
+                                    if(err){
+                                        console.log(err);
+                                        //callback();
+                                        return res.json(200, {status: 2, message: "Failure"
+                                                        });
+                                    }else{
+                                            //console.log("Results ---------- >>>>>>>>>");
+                                            console.log(results);
+                                             return res.json(200, {status: 1, message: "success",
+                                                                    result: results
+                                                        });
+                                    }
 
                         });
     },

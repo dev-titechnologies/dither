6a935f3a16c2526dbc8246f6ba73fe5e874d8133
Sojimon <sojimon.cy@titechnologies.org>
2017-01-23 19:26:36 +0530

    added default dither changing option

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -770,8 +770,9 @@ console.log(values);
 						var TOKEN_EXPIRY_HOUR					=		req.options.settingsKeyValue.TOKEN_EXPIRY_HOUR;
 						var DITHER_EXPIRY_HOUR					=		req.options.settingsKeyValue.DITHER_EXPIRY_HOUR;
 						var image;
-						console.log(TOKEN_EXPIRY_HOUR);
-						console.log(DITHER_EXPIRY_HOUR);
+						//console.log(TOKEN_EXPIRY_HOUR);
+						//console.log(DITHER_EXPIRY_HOUR);
+						//console.log(default_collage4_Array);
 						default_collage1_Array.forEach(function(factor, index){
 							image				=	collageImg_path+factor.image;
 							factor.image		=	image;
@@ -982,21 +983,89 @@ updateTokenExpiryTime:function(req,res){
                             }
     },
 	changeDefaultDither	:	function(req,res){
-							 var imageUploadDirectoryPath =  '../../assets/images/test';
-							console.log(req.params.all());
-							req.file('acoconut').upload({dirname:imageUploadDirectoryPath, maxBytes: 10000000},function (err, files) {
-								if (err){
-									console.log("cannot upload not"+err);
-									//callback();
-								}
-								else{
-								   console.log("Uploaded"+files);
-								   return res.json({message: files.length + ' file(s) uploaded successfully!',files: files});
-								}
-							});
+								var imageUploadDirectoryPath =  '../../assets/images/collage';
+								//console.log(req);
+								console.log(req.params.all());
+								//console.log(req.params.SubmitIdentity);
+								var SubmitIdentity = req.param("SubmitIdentity");
+								
+								//console.log(req.file('Multiplefile')._files[0].stream);
+								//console.log(req.body.file2);
+								var collageArray 	=	[];
+								var image;
+								var position;
+								var value =	[];
+								var criteria;
+								console.log("inside change default dither=============");
+								req.file('Multiplefile').upload({dirname:imageUploadDirectoryPath, maxBytes: 100000000},function (err, files) {
+									if (err){
+										console.log("cannot upload not"+err);
+										//callback();
+									}
+									else{
+										//console.log("file1111111111111111111111111111111");
+										console.log(files);
+										//console.log(JSON.parse(file1));
+										//console.log("file10000000000000000000000000000000");
 
-	},
-	
+										files.forEach(function(factor, index){
+											 var uploadedfilename                   =    factor.fd.split('/');
+                                             uploadedfilename                       =    uploadedfilename[uploadedfilename.length-1];
+                                             collageArray.push({       
+																	image       : 	uploadedfilename,
+																	position	:	index
+																	
+																	});
+											});
+											//console.log(collageArray);
+											var data		=	JSON.stringify(collageArray);
+											//console.log(data);
+										//if()
+										/*var criteria		=	{
+																	key	:	"DEFAULT_DITHER_FOUR"
+																}*/
+										  switch(SubmitIdentity){
+																		case "1":  criteria = {		key	:	"DEFAULT_DITHER_FOUR"	}
+																					console.log("inside case 1******************")
+																					break;
+																		case "2":
+																					criteria = {		key	:	"DEFAULT_DITHER_THREE"	}
+																					console.log("inside case 2******************")
+																					break;
+																		case "3":   criteria = {		key	:	"DEFAULT_DITHER_TWO"	}
+																					console.log("inside case 3******************")
+																					break;
+																		case "4":  criteria = {	key	:	"DEFAULT_DITHER_ONE"	}
+																					console.log("inside case 4******************")
+																					break;
+																}
+										var data            =   {
+																	value:	data
+																}
+											//console.log("criteria")
+											//console.log(criteria)
+											console.log("SubmitIdentity")
+											console.log(SubmitIdentity)
+											console.log("SubmitIdentity")
+											//console.log("data")
+											//console.log(data)
+											Settings.update(criteria,data).exec(function(err,updatedData){
+												if(err)
+												{
+													 console.log("collage Images can not inserted into table");
+													 return res.json(200, {status: 2, error_details: err});
+												}
+												else
+												{
+													console.log("collage Images inserted into table");
+													console.log(updatedData);
+													return res.json(200,{status:1,message:'success',result:updatedData});
+												}
+											});
+										//return res.json(200,{status:1,message:'success',result:1});
+										}
+		
 
-};
-
+								});
+							},
+	}

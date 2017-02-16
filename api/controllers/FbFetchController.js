1d56b92809cefbb7f4d633cf3eec760d9ea6d4ff
anumol rajeev <anumol.rajeev@titechnologies.org>
2017-02-16 11:47:11 +0530

    push changes when update fb friendss list

--- api/controllers/FbFetchController.js
+++ api/controllers/FbFetchController.js
@@ -23,19 +23,22 @@ module.exports = {
 		  var frnds	 	    	= [];
 		  var frnds_arr			= [];
 		  var fbUserArray 		= [];
-		  var params 			= req.params.all();
+		  var params 			= { entry: [ { time: 1487224915,id: '119124805202604', changed_fields: [Object],uid: '119124805202604' } ],object: 'user' };
+		 // var params = {};
 		  console.log("rececive dataaaaaaaaaaaaa")
 		  console.log(params)
-		  console.log(params.length)
+		  var data			= JSON.stringify(params);
+		  console.log(data)
+		  console.log(data.length)
 		  //var data	=	'{"entry":[{"time":1487051631,"id":"129701107473387","changed_fields":["friends"],"uid":"129701107473387"}],"object":"user"}';
 		  //var data  = {};
-		  if(params.length==undefined){
+		  if(data.length==undefined){
 			            
 			  return res.json(200, {status: 2, status_type: 'Failure',message:'no data found'});
 			   //return res.send(challenge);
 		  }else{ 
 		     console.log("dataaaaaaaaaaa")
-			 var data			= JSON.stringify(params);
+			 
 			 console.log(data)
 			 var fbData 	 	= JSON.parse(data);
 			 if(fbData.entry.length){ 
